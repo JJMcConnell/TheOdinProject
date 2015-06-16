8c3c8bc157c068cd383ea12220db49055fcf95d4
Joseph McConnell <JJMcConnell17@gmail.com>
2015-06-15 22:52:14 -0400

    Trying to figure out the color change

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 var container = $('.container');
 
 var num = 1;
-for (var i =0; i < 20; i++) {
-	for (var j=0; j < 20; j++) {
+for (var i =0; i < 16; i++) {
+	for (var j=0; j < 16; j++) {
 
 	container.append( $('<div id="div' + num + ' "></div>').addClass('box'));
 	num++;
@@ -12,13 +12,27 @@ for (var i =0; i < 20; i++) {
 }
 
 
-var width = 550 / 22;
-var height = 550 / 22;
+var width = 550 / 16;
+var height = 550 / 16;
+var boxClass = $('.box')
 
-$('.box').css({"width": "" + width + "px",
+boxClass.css({"width": "" + width + "px",
 			  "height": "" + height + "px"});
 
+/* 
+$('#div1').mouseenter(function() {
 
+	$('this').removeClass('box').addClass('blackBox');
 
+	console.log("mouseenter fired");
+
+});
+
+boxClass.mouseleave(function() {
+
+	$('this').css({"background": "black"});
+
+});
+*/
 
 });
