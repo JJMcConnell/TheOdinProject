8586308a9dda983b55a5e2b3b34063dbeb7510ce
Joseph McConnell <JJMcConnell17@gmail.com>
2015-08-11 13:18:10 -0400

    Can now tweet out shortened urls

--- twitter_bot/micro_blogger.rb
+++ twitter_bot/micro_blogger.rb
@@ -1,4 +1,7 @@
 require 'jumpstart_auth'
+require 'bitly'
+
+Bitly.use_api_version_3
 
 class MicroBlogger
 	attr_reader :client
@@ -56,6 +59,12 @@ class MicroBlogger
 		end
 	end
 
+	def shorten(original_url)
+		bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+		puts "Shortening this URL: #{original_url}"
+		bitly.shorten(original_url).short_url
+	end
+
 
 	def run
 		puts "Welcome to the JSL Twitter Client!"
@@ -72,6 +81,8 @@ class MicroBlogger
 				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
 				when 'spam' then spam_my_followers(parts[1..-1].join(" "))
 				when 'elt' then everyones_last_tweet
+				when 's' then shorten(parts[1..-1].join(" "))
+				when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 				else
 					puts "Sorry, I don't know how to #{command}" 
 			end
